157565c3e0be1a9360785c3e07d913e05734e737
Dayanand Chinchure <102784873+dchinchure-cr@users.noreply.github.com>
2023-10-03 16:43:10 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@ echo $2
 export GOOGLE_CLOUD_PROJECT=$1
 export CHECKRED_SERVICE_ACCOUNT_EMAIL=$2
 gcloud services enable iamcredentials.googleapis.com --project $1
+gcloud services enable cloudresourcemanager.googleapis.com --project $1
 sed -i "s/PROJECT_ID/$1/g" checkred.tf
 sed -i "s/CHECKRED_SERVICE_ACCOUNT_EMAIL/$2/g" checkred.tf
 terraform init
