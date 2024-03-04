ffde875959ad3e450869a153bf4f76114fbf7980
Aniket Chavan <121092544+achavan-cr@users.noreply.github.com>
2024-03-04 20:33:10 +0530

    fetch org_id in json

--- org-onboarding/org-onboarding.sh
+++ org-onboarding/org-onboarding.sh
@@ -1,11 +1,11 @@
 echo $1
 echo $2
-export GOOGLE_CLOUD_PROJECT=$1
-export CHECKRED_SERVICE_ACCOUNT_EMAIL=$2
+export PROJECT_ID=$1
+export CHECKRED_ORG_SERVICE_ACCOUNT_EMAIL=$2
 gcloud services enable iamcredentials.googleapis.com --project $1
 gcloud services enable cloudresourcemanager.googleapis.com --project $1
-export ORG_ID=`gcloud projects get-ancestors $1 | grep organization | awk '{print $1;}'`
-sed -i "s/PROJECT_ID/$1/g" versions.tf
+export ORG_ID=`gcloud projects get-ancestors $PROJECT_ID --format=json | jq -r '.[] | select(.type == "organization") | .id'`
+sed -i "s/PROJECT_ID/$PROJECT_ID/g" versions.tf
 sed -i "s/ORGANIZATION_ID/$ORG_ID/g" checkred_org_integration.tf
 sed -i "s/CHECKRED_ORG_SERVICE_ACCOUNT_EMAIL/$2/g" checkred_org_integration.tf
 terraform init
