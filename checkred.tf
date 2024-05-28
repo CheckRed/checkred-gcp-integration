7eb4d70e971dbf9711343bbecd65b632d6a9dc66
Aniket Chavan <121092544+achavan-cr@users.noreply.github.com>
2024-05-28 19:37:40 +0530

    added gke permission

--- checkred.tf
+++ checkred.tf
@@ -27,3 +27,24 @@ resource "google_project_iam_binding" "token_creator_binding" {
     "serviceAccount:CHECKRED_SERVICE_ACCOUNT_EMAIL",
   ]
 }
+
+resource "google_project_iam_custom_role" "checkred_gke_policy" {
+  role_id     = "checkred_gke_policy"
+  title       = "CheckRed GKE Policy"
+  description = "Custom role with permission to proxy GKE nodes"
+  project     = "PROJECT_ID"
+
+  permissions = [
+    "container.nodes.proxy",
+  ]
+}
+
+resource "google_project_iam_binding" "checkred_gke_policy_binding" {
+  project = "PROJECT_ID"
+  role    = "projects/${var.project_id}/roles/${google_project_iam_custom_role.checkred_gke_policy.role_id}"
+
+  members = [
+    "serviceAccount:CHECKRED_SERVICE_ACCOUNT_EMAIL",
+  ]
+}
+
