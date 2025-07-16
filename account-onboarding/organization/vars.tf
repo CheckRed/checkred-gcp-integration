39b9ab3be32ec1f4ab22944490f12dbb96b3b1a3
Aniket Chavan <121092544+achavan-cr@users.noreply.github.com>
2025-07-16 15:44:58 +0530

    CHEC-18758 | AC |  vars for cspm & dnspm
    Signed-off-by: Aniket Chavan <121092544+achavan-cr@users.noreply.github.com>

---
+++ account-onboarding/organization/vars.tf
@@ -0,0 +1,11 @@
+variable "enable_cspm" {
+  description = "Enable CSPM onboarding"
+  type        = bool
+  default     = false
+}
+
+variable "enable_dnspm" {
+  description = "Enable DNSPM onboarding"
+  type        = bool
+  default     = false
+} 
