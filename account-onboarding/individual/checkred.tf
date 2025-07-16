9f2e4c237d3c11aa54d242830ce8cd6b0bef5b9b
Aniket Chavan <121092544+achavan-cr@users.noreply.github.com>
2025-07-16 16:04:47 +0530

    CHEC-18758 | AC | individual account onboarding
    Signed-off-by: Aniket Chavan <121092544+achavan-cr@users.noreply.github.com>

---
+++ account-onboarding/individual/checkred.tf
@@ -0,0 +1,66 @@
+resource "google_service_account" "checkred_integration" {
+  account_id   = "checkred-integration"
+  display_name = "CheckRed Integration"
+  project      = "PROJECT_ID"
+}
+
+#Output the email of the service account
+output "service_account_email" {
+  value = google_service_account.checkred_integration.email
+}
+
+resource "google_project_iam_binding" "viewer_binding" {
+  count   = var.enable_cspm ? 1 : 0
+  project = "PROJECT_ID"
+  role    = "roles/viewer"
+
+  # Reference the email of the service account from the output
+  members = [
+    "serviceAccount:${google_service_account.checkred_integration.email}",
+  ]
+}
+
+resource "google_project_iam_custom_role" "checkred_dnspm_viewer_role" {
+  count       = var.enable_dnspm ? 1 : 0
+  role_id     = "CheckRedDNSPMRole"
+  title       = "CheckRed DNSPM Viewer Role"
+  description = "Custom role with DNSPM specific permissions"
+  permissions = [
+    "dns.changes.get",
+    "dns.changes.list",
+    "dns.dnsKeys.get",
+    "dns.dnsKeys.list",
+    "dns.managedZoneOperations.get",
+    "dns.managedZoneOperations.list",
+    "dns.managedZones.get",
+    "dns.managedZones.list",
+    "dns.policies.get",
+    "dns.policies.list",
+    "dns.projects.get",
+    "dns.resourceRecordSets.get",
+    "dns.resourceRecordSets.list",
+    "resourcemanager.projects.get",
+    "logging.logEntries.list",
+    "logging.privateLogEntries.list"
+  ]
+}
+
+resource "google_project_iam_binding" "dns_logs_role_viewer_binding" {
+  count   = var.enable_dnspm ? 1 : 0
+  project = "PROJECT_ID"
+  role    = google_project_iam_custom_role.checkred_dnspm_viewer_role[0].name
+
+  # Reference the email of the service account from the output
+  members = [
+    "serviceAccount:${google_service_account.checkred_integration.email}",
+  ]
+}
+
+resource "google_project_iam_binding" "token_creator_binding" {
+  project = "PROJECT_ID"
+  role    = "roles/iam.serviceAccountTokenCreator"
+
+  members = [
+    "serviceAccount:CHECKRED_SERVICE_ACCOUNT_EMAIL",
+  ]
+}
