e556b98538ae22ee61f6e5ff69a007776d4de475
rahulrajdevops-cr <rahul.raj@checkred.com>
2025-12-09 18:39:56 +0530

    Update checkred.tf
    Signed-off-by: rahulrajdevops-cr <rahul.raj@checkred.com>

--- checkred.tf
+++ checkred.tf
@@ -1,25 +1,35 @@
-resource "google_service_account" "checkred_integration" {
-  account_id   = "checkred-integration-account"
-  display_name = "CheckRed Integration"
+resource "google_service_account" "checkred_dns_integration" {
+  account_id   = "checkred-dns-integration-account"
+  display_name = "CheckRed DNS Integration"
   project      = "PROJECT_ID"
 }
 
 # Output the email of the service account
 output "service_account_email" {
-  value = google_service_account.checkred_integration.email
+  value = google_service_account.checkred_dns_integration.email
 }
 
-resource "google_project_iam_binding" "viewer_binding" {
+resource "google_project_iam_binding" "dns_reader_binding" {
   project = "PROJECT_ID"
-  role    = "roles/viewer"
+  role    = "roles/dns.reader"
 
   # Reference the email of the service account from the output
   members = [
-    "serviceAccount:${google_service_account.checkred_integration.email}",
+    "serviceAccount:${google_service_account.checkred_dns_integration.email}",
   ]
 }
 
-resource "google_project_iam_binding" "token_creator_binding" {
+resource "google_project_iam_binding" "dns_logs_viewer_binding" {
+  project = "PROJECT_ID"
+  role    = "roles/logging.viewer"
+
+  # Reference the email of the service account from the output
+  members = [
+    "serviceAccount:${google_service_account.checkred_dns_integration.email}",
+  ]
+}
+
+resource "google_project_iam_binding" "token_dns_creator_binding" {
   project = "PROJECT_ID"
   role    = "roles/iam.serviceAccountTokenCreator"
 
