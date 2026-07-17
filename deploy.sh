f022c9336d9ba227c5cfc4ac43e9957f1cc499a6
Rahul Raj <rahul.raj@checkred.com>
2026-07-17 15:33:37 +0530

    Update deploy.sh
    Signed-off-by: Rahul Raj <rahul.raj@checkred.com>

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,8 @@ export GOOGLE_CLOUD_PROJECT=$1
 export CHECKRED_SERVICE_ACCOUNT_EMAIL=$2
 gcloud services enable iamcredentials.googleapis.com --project $1
 gcloud services enable cloudresourcemanager.googleapis.com --project $1
-sed -i "s/PROJECT_ID/$1/g" checkred.tf
-sed -i "s/CHECKRED_SERVICE_ACCOUNT_EMAIL/$2/g" checkred.tf
+sed -i "s/PROJECT_ID/$1/g" gcp.tf
+sed -i "s/CHECKRED_SERVICE_ACCOUNT_EMAIL/$2/g" gcp.tf
 terraform init
 terraform plan
 terraform apply -auto-approve
